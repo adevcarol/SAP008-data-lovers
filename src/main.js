98e687f4e11b53aca7ac60b6c1f93ca987afb62a
Juhreisf <109034395+Juhreisf@users.noreply.github.com>
2022-09-01 14:36:53 -0300

    filtro medalhas luta

--- src/main.js
+++ src/main.js
@@ -1,28 +1,93 @@
 // import { example } from './data.js';
+import athletes from './data/athletes/athletes.js';
 import data from './data/athletes/athletes.js';
 
 // console.log(data);
 
 const content = document.querySelector('.cards-main');
+const medalluta = document.querySelector('#medalhasluta');
 
-// const data2 = [data.athletes[0], data.athletes[1], data.athletes[2]]
-const arrSports = data.athletes
+const arrAthletes = data.athletes
 
-arrSports.forEach((filtersport, index) => { // inicio do forEach
-    if (filtersport.sport === "Wrestling") {
-        const template = `
-            <div class="card-atleta" id="card-atleta">
-                <div class="atributos-atleta">
+// Filtro por medalhas 
+medalluta.addEventListener('change', (event) => {
+    console.log(event.target.value)
+    let medallutaType = event.target.value
 
-                    <p class="esporte">${element.sport}</p>
-                    
-                </div>
-            </div>
-            `
-        content.innerHTML += template
+    if (medallutaType == "") {
+        arrAthletes.map((filterluta) => {
+            if (filterluta.sport == "Wrestling") {
+                const template = `
+        <div class="card-atleta" id="card-atleta">
+            <div class="atributos-atleta">
+                <p class="nome">${filterluta.name}</p>
+                <p class="genero">${filterluta.gender}</p>
+                <p class="esporte">${filterluta.sport}</p>
+                <p class="esporte">${filterluta.medal}</p>
+    </div>
+    </div>  
+    `
+                content.innerHTML += template
+            }
+
+        })
     }
-    console.log(filtersport.name)
 
-});
+    else if (medallutaType == "Gold") {
+        arrAthletes.map((filterluta) => {
+            if (filterluta.medal == "Gold" && filterluta.sport == "Wrestling") {
+                const template = `
+        <div class="card-atleta" id="card-atleta">
+            <div class="atributos-atleta">
+                <p class="nome">${filterluta.name}</p>
+                <p class="genero">${filterluta.gender}</p>
+                <p class="esporte">${filterluta.sport}</p>
+                <p class="esporte">${filterluta.medal}</p>
+    </div>
+    </div>  
+    `
+                content.innerHTML += template
+            }
 
 
+        })
+    }
+
+    else if (medallutaType == "Silver") {
+        arrAthletes.map((filterluta) => {
+            if (filterluta.medal == "Silver" && filterluta.sport == "Wrestling") {
+                const template = `
+        <div class="card-atleta" id="card-atleta">
+            <div class="atributos-atleta">
+                <p class="nome">${filterluta.name}</p>
+                <p class="genero">${filterluta.gender}</p>
+                <p class="esporte">${filterluta.sport}</p>
+                <p class="esporte">${filterluta.medal}</p>
+    </div>
+    </div>  
+    `
+                content.innerHTML += template
+
+            }
+
+        })
+    }
+
+    else if (medallutaType == "Bronze") {
+        arrAthletes.map((filterluta) => {
+            if (filterluta.medal == "Bronze" && filterluta.sport == "Wrestling") {
+                const template = `
+        <div class="card-atleta" id="card-atleta">
+            <div class="atributos-atleta">
+                <p class="nome">${filterluta.name}</p>
+                <p class="genero">${filterluta.gender}</p>
+                <p class="esporte">${filterluta.sport}</p>
+                <p class="esporte">${filterluta.medal}</p>
+    </div>
+    </div>  
+    `
+                content.innerHTML += template
+            }
+        })
+    }
+})
